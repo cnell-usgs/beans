b0b2479b9cf0e07cb3f1983c520030991017ae5c
collnell <coll.s.nell#gmail.com>
2016-09-27 17:39:45 -0700

    table and mean plot synced with proportion calc and reactive error bars

--- server.R
+++ server.R
@@ -20,22 +20,11 @@ shinyServer(function(input,output){
   output$var2<-renderText({input$var2})
   
   ##make blank dfs
-  testdf1 = data.frame(variable1 = as.numeric(runif(20,0,20)),
-                      variable2 = as.numeric(runif(20,0,30)))
-  testdf1$propvar1<-testdf1$variable1/(testdf1$variable1+testdf1$variable2)
-  
-  testdf2 = data.frame(variable1 = as.numeric(runif(20,0,20)),
-                       variable2 = as.numeric(runif(20,0,30)))
-  testdf2$propvar1<-testdf2$variable1/(testdf2$variable1+testdf2$variable2)
   
   values <- reactiveValues(df_data1 = data.frame(variable1 = as.numeric(rep(NA,20)),
                                                 variable2 = as.numeric(rep(NA,20))),
                            df_data2 =data.frame(variable1 = as.numeric(rep(NA,20)),
                                                 variable2 = as.numeric(rep(NA,20))))
-  values$testdf1 = data.frame(variable1 = as.numeric(runif(20,0,20)),
-                              variable2 = as.numeric(runif(20,0,30)))
-  values$testdf2 = data.frame(variable1 = as.numeric(runif(20,0,20)),
-                              variable2 = as.numeric(runif(20,0,30)))
   values$df_data = data.frame(variable1= as.numeric(rep(0,40)),
                                   variable2=as.numeric(rep(0,40)),
                                   treat = as.factor(rep("treat1",40)),
@@ -43,7 +32,9 @@ shinyServer(function(input,output){
   values$summ_data = data.frame(treat = c("treat1","treat2"),
                                 mean_prop = c(0,0),
                                 n_df = c(0,0),
-                                errorr = c(0,0))
+                                se_prop = c(0,0),
+                                sd_prop = c(0,0),
+                                ci_prop = c(0,0))
   
   ##generate talbes
   output$table1 = renderRHandsontable({
@@ -55,39 +46,45 @@ shinyServer(function(input,output){
     rhandsontable(values$df_data2,colHeaders =c(input$var1,input$var2,pvar1))
   })
   
-  observeEvent(input$getdata, {
-    df_data1<-hot_to_r(input$table1)
+  observeEvent(input$getdata, { #an error occurs if there are empty cells ' must be vector type was null'
+    df_data1<-hot_to_r(input$table1)##needs some kind of remove NA
     df_data2<-hot_to_r(input$table2)
+    
+    df_data2
     
     df_data1$treat<-rep(input$treat1,length(values$df_data1$variable1))
     df_data2$treat<-rep(input$treat2,length(values$df_data2$variable1))
     
     df_data_all<-rbind(df_data1,df_data2)
-    df_data_all$prop<-df_data_all$variable1/(df_data_all$variable1/df_data_all$variable2)
+    df_data_all$prop<-df_data_all$variable1/(df_data_all$variable1+df_data_all$variable2)
     
-    errorb<-switch(input$errortype,##reactive error bars
-                   se=se,
-                   sd=sd,
-                   ci=ci95)
-    
-    summdata<-df_data_all%>%
+    summdata<-df_data_all%>%#summarize data
+      na.omit()%>%
       group_by(treat)%>%
-      summarize(mean_prop = mean(prop), n_df = length(prop), errorr=errorb(prop))
+      summarize(mean_prop = mean(prop), 
+                n_df = length(prop), 
+                se_prop=se(prop), 
+                sd_prop = sd(prop), 
+                ci_prop = ci95(prop))
     
     values$df_data<-df_data_all
     values$summ_data<-summdata
-    str(df_data1)
   })
 
  #plot means
   output$plot1 <- renderPlot({
+    error<-switch(input$errortype,##reactive error bars
+                   se=values$summ_data$se_prop,
+                   sd=values$summ_data$sd_prop,
+                   ci=values$summ_data$ci_prop)
+    
     yvar.lab<-paste("Proportion",input$var1)#reactive y axis label based on variable inputs
     ##eventually want the plotted variable to be a drop-down so that it could be proportion var1 or var2
     
     bar.plot<-ggplot(values$summ_data,aes(x=treat,y=mean_prop,fill=treat))+#plot
       geom_bar(stat="identity")+
       theme_mooney()+
-      geom_errorbar(aes(ymax=mean_prop+errorr,ymin=mean_prop-errorr),width=.2)+
+      geom_errorbar(aes(ymax=mean_prop+error,ymin=mean_prop-error),width=.2)+
       labs(x="Treatment",y=yvar.lab)+
       scale_fill_manual(values=c("#006666","#FF9900"))+
       theme(legend.position="none")
